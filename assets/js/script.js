60049d9f5763b8a89f8cdd7871ffc380d4ec2acf
Gordon-Magill <gordon.magill@gmail.com>
2022-08-23 19:37:15 -0700

    Refined grading logic + began adding high score table

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ var resultPage = document.querySelector('#resultPage');
 var startButton = document.querySelector('#startButton');
 startButton.addEventListener('click', playGame);
 
-
 // The main question prompt on the quiz page
 var questionPrompt = document.querySelector("#questionPrompt")
 
@@ -33,9 +32,15 @@ var allCheckBoxes = document.querySelectorAll("input[type='checkbox']");
 // The text element stating the score of the current completed quiz
 var userScore = document.querySelector('#userScore')
 
+// Message on the results page
+var resultMessage = document.querySelector('#resultMessage')
+
 // The button for submitting an answer to one of the questions
 var submitAnswerButton = document.querySelector("#submitAnswer")
 submitAnswerButton.addEventListener('click', gradeAnswer);
+
+// Timer <span> value shown to the user
+var timerTimeLeft = document.querySelector("#timerTimeLeft")
 
 // Elements that handle the user submitting a score for ranking
 var initialForm = document.querySelector('#userInitials');
@@ -45,7 +50,9 @@ submitButton.addEventListener('click', submitScore)
 // Basic setup variables for the quiz
 numQuestions = 10; //Number of questions to be asked on the quiz
 timerMaxDuration = 60; //Duration in seconds
+var timerValue = timerMaxDuration; //Counter variable
 correctAnswers = 0; //Counter for the correctly answered questions
+timerPenalty = 5; //Penalize time for an incorrect guess
 
 // Questions, possible answers, and the correct answer encoded as the 
 questionsAnswersRef = [
@@ -145,12 +152,11 @@ function shuffle(array) {
 function playGame() {
     startPage.style.display = 'none';
     quizPage.style.display = "flex";
-    var timerValue = timerMaxDuration;
-
 
     refreshQuestion()
     setInterval(function() {
         timerValue-- //Decrement timer by 1
+        timerTimeLeft.textContent = timerValue;
         if (timerValue == 0) {
             endGame()
         }
@@ -175,7 +181,7 @@ function setCorrectAnswer(question) {
             allCheckBoxes[i].setAttribute('data-correct','false')
         }
         
-        console.log("Set question "+i+" to "+allCheckBoxes[i].getAttribute('data-correct'))
+        // console.log("Set question "+i+" to "+allCheckBoxes[i].getAttribute('data-correct'))
     }
 }
 
@@ -186,7 +192,8 @@ function refreshQuestion() {
     // If there are no more questions, end the game
     questionLength = questionsAnswers.length;
     if (questionLength<1) {
-        endGame()
+        endGame();
+        return;
     }
 
     // If there are more questions to answer, refresh the page content with the
@@ -212,15 +219,16 @@ function gradeAnswer(event) {
     event.preventDefault();
     var checkedAnswers = document.querySelectorAll('input:checked')
 
-    console.log("Checked answers:")
-    console.log(checkedAnswers)
-    console.log("Checked answer status:")
-    console.log(checkedAnswers[0].getAttribute('data-correct'))
+    // console.log("Checked answers:")
+    // console.log(checkedAnswers)
+    // console.log("Checked answer status:")
+    // console.log(checkedAnswers[0].getAttribute('data-correct'))
 
     if (checkedAnswers.length>1) {
         // More than one answer was submitted, which is wrong
         console.log('Submitted more than one answer, incorrect')
         refreshQuestion();
+        timerValue -= timerPenalty;
     } else if (checkedAnswers[0].getAttribute('data-correct')=='true') {
         // Compare if the only selected answer was the correct one
         correctAnswers++
@@ -229,7 +237,9 @@ function gradeAnswer(event) {
     } else {
         console.log('No correct answer detected')
         refreshQuestion()
+        timerValue -= timerPenalty;
     }
+
     
 
 }
@@ -241,9 +251,102 @@ function endGame() {
 
 }
 
-// Updates the 
-function submitScore() {
-    return;
+var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+console.log('Existing High Scores:')
+console.log(highScores)
+
+// Updates the high scores page and storage after user submits their score
+function submitScore(event) {
+    event.preventDefault()
+
+    userScore = {
+        userInitials: initialForm.value,
+        userPoints: correctAnswers
+    }
+
+    function scoreHelper() {
+        console.log('Acivated scoreHelper()')
+
+        // Sort the scores
+        highScores.sort(scoreSorter)
+
+        // Only store the top 10 scores
+        highScores = highScore.slice(0,9)
+
+        // Save the new high scores to localStorage
+        localStorage.setItem('highScores',JSON.stringify(highScores))
+
+        // Render the new high scores
+        refreshHighScores()
+
+        // Change the results page message
+        resultMessage.textContent = "Your score has been submitted!"
+        submitButton.disabled = true;
+    }
+
+    // If a the same user/score pair is already on the leaderboard, ignore it
+    // for (const score in highScores){
+    //     if (score.userInitials === userScore.userInitials && score.userPoints === userScore.userPoints) {
+    //         console.log("Found replicate score");
+    //         scoreHelper();
+    //         return;
+    //     }
+    // }
+
+    // If there are less than 10 scores, simply add it
+    if (highScores.length < 10) {
+        console.log('Less than 10 scores found, adding the score')
+        highScores.push(userScore)
+        scoreHelper()
+        return
+
+    } else {
+        //In cases where there are already scores, add userScore if it beats out the worst highscore
+
+        // Find the current max and min score amongst those that are stored
+        maxScore = 0;
+        minScore = numQuestions;
+        for (const score in highScores) {
+            if (score.userPoints > maxScore) {
+                maxScore = score.userPoints
+            } else if (score.userPoints < minScore) {
+                minScore = score.userPoints;
+            }
+        }
+
+        // If the current score is greater than the min score, add it to highScores
+        if (userScore.userPoints > minScore) {
+            highScores.push(userScore)
+        }
+
+        // Sorting function for high scores, sorting by the point value
+        function scoreSorter(a,b) {
+            if (a.userPoints < b.userPoints) {
+                return 1
+            } else if (a.userPoints > b.userPoints) {
+                return -1
+            } else {
+                return 0
+            }
+        }
+
+        
+
+        scoreHelper()
+        
+    }
+
+}
+
+function refreshHighScores() {
+
+    var tableHeader = document.createElement('<tr>')
+    tableHeader.innerHTML = '<tr><th>Initials</th><th>Score</th></tr>'
+
+    var tableHolder = document.querySelector('#highScoreTable')
+
+
+
 }
 
 // Initialize the game questions
